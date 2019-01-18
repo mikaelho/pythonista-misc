66dbad19370320d5d1b371462dd178c7cec7e220
Mikael <mikael.honkala@gmail.com>
2019-01-18 08:38:53 +0200

    Themes

--- timer.py
+++ timer.py
@@ -1,4 +1,26 @@
-import ui, console
+import ui, console, objc_util, keychain
+from types import SimpleNamespace as ns
+
+font = 'Apple SD Gothic Neo'
+UIColor = objc_util.ObjCClass('UIColor')
+objc_black = UIColor.darkGrayColor().CGColor()
+
+light_theme = ns(front='black', back='white', secondary='darkgrey', shadow=objc_black)
+dark_theme = ns(front='white', back='black', secondary='grey', shadow=objc_black)
+blue_theme = ns(front='#1976D2', back='white', secondary='#03A9F4', shadow=objc_black)
+green_theme = ns(front='#009688', back='white', secondary='#80CBC4', shadow=objc_black)
+red_theme = ns(front='#E53935', back='white', secondary='#FFA726', shadow=objc_black)
+cyan_dark_theme = ns(front='#4DD0E1', back='black', secondary='#00897B', shadow=objc_black)
+
+themes = [light_theme, dark_theme, blue_theme, green_theme, red_theme, cyan_dark_theme]
+
+try:
+  current_theme_index = int(keychain.get_password('timer','timer'))
+  theme = themes[current_theme_index]
+except TypeError:
+  current_theme_index = 0
+  theme = light_theme
+
 
 class Toucher(ui.View):
 
@@ -20,7 +42,7 @@ class Toucher(ui.View):
       self.update_interval = 0
       self.running = False
       self.seconds = 60
-      l.text_color = 'darkgrey'
+      l.text_color = theme.secondary
       console.set_idle_timer_disabled(False)
       return
     self.seconds -= 1
@@ -30,7 +52,7 @@ class Toucher(ui.View):
       self.current_time -= 1
 
   def draw(self):
-    ui.set_color('black')
+    ui.set_color(theme.front)
     path = ui.Path()
     path.line_width = 10
     insets = self.objc_instance.safeAreaInsets()
@@ -74,11 +96,11 @@ class Toucher(ui.View):
       self.running = self.running == False
       if self.running:
         self.update_interval = 1
-        l.text_color = 'black'
+        l.text_color = theme.front
         console.set_idle_timer_disabled(True)
       else:
         self.update_interval = 0
-        l.text_color = 'darkgrey'
+        l.text_color = theme.secondary
         console.set_idle_timer_disabled(False)
     self.panning = False
     self.prev_y = 0
@@ -87,63 +109,20 @@ class Toucher(ui.View):
 max_size = min(ui.get_screen_size())
 timer_font_size = 1000
 while timer_font_size > 1:
-  w, h = ui.measure_string('99', font=('Courier', timer_font_size), alignment=ui.ALIGN_CENTER)
+  w, h = ui.measure_string('OO', font=(font, timer_font_size), alignment=ui.ALIGN_CENTER)
   if w < max_size: break
-  if w/max_size > 1.2:
-    timer_font_size /= 1.2
-  else:
-    timer_font_size -= 1
+  #if w/max_size > 1.2:
+  timer_font_size /= 1.1
+  #else:
+  #  timer_font_size -= 1
     
 class TimeSelection(ui.View):
   
-  default_times = (1, 2, 3, 5, 10, 15, 20, 30, 45, 60, 75, 90)
+  default_times = ('theme', 1, 2, 3, 5, 10, 15, 20, 30, 45, 60, 90)
   times_per_row = 3
   rows = 4
   cell_font = timer_font_size/(times_per_row+1)
   first_time = True
-
-  @property 
-  def masks_to_bounds(self):
-    return self.pntr.layer().masksToBounds()
-        
-  @masks_to_bounds.setter
-  def masks_to_bounds(self, val):
-    self.pntr.layer().setMasksToBounds_(val)
-  
-  @property
-  def shadow_opacity(self):
-    return self.pntr.layer().shadowOpacity()
-        
-  @shadow_opacity.setter
-  def shadow_opacity(self, val):
-    self.pntr.layer().setShadowOpacity_(val)
-        
-  @property
-  def shadow_radius(self):
-    return self.pntr.layer().shadowRadius()
-        
-  @shadow_radius.setter
-  def shadow_radius(self, val):
-    self.pntr.layer().setShadowRadius_(val)
-        
-  @property
-  def shadow_offset(self):
-    return self.pntr.layer().shadowOffset()
-        
-  @shadow_offset.setter
-  def shadow_offset(self, offset):
-    self.pntr.layer().setShadowOffset_(CGSize(*offset))
-            
-  @property
-  def shadow_color(self):
-    return self.pntr.layer().shadowColor()
-        
-  @shadow_color.setter
-  def shadow_color(self, color):
-    (red, green, blue, alpha) = parse_color(color)
-    objc_color = ObjCClass('UIColor').colorWithRed_green_blue_alpha_(red, green, blue, alpha).CGColor()
-    
-    self.pntr.layer().setShadowColor_(objc_color)
     
   def set_drop_shadow(self, color):
     self.shadow_opacity = 1
@@ -157,18 +136,47 @@ class TimeSelection(ui.View):
     for i in range(len(self.default_times)):
       time_value = self.default_times[i]
       b = ui.Button(
-        title=str(time_value), 
         tint_color='black',
-        border_color='lightgrey',
-        border_width=1,
+        #tint_color='white',
+        background_color='white',
+        #border_color='lightgrey',
+        #border_width=1,
         corner_radius=10,
         flex='WH')
-      b.font = ('Courier', self.cell_font)
-      if i % 2 == 1:
-        b.tint_color = 'grey'
+      if time_value == 'theme':
+        b.image = ui.Image('iob:waterdrop_32')
+        b.action = self.toggle_theme
+      else:
+        b.title = str(time_value)
+        b.action = self.go_to_timer
+      #b.objc_instance.setClipsToBounds_(False)
+      bl = b.objc_instance.layer()
+      bl.setMasksToBounds_(False)
+      bl.setShadowOpacity_(1)
+      bl.setShadowRadius_(1)
+      bl.setShadowOffset_(objc_util.CGSize(2, 2))
+      bl.setShadowColor_(objc_black)
+      
+      b.font = (font, self.cell_font)
+      #if i % 2 == 1:
+      #  b.background_color = 'darkgrey'
       self.times.append(b)
-      b.action = self.go_to_timer
-      self.add_subview(b)      
+      self.add_subview(b)     
+    self.set_theme() 
+     
+  def toggle_theme(self, sender):
+    global theme, current_theme_index, themes
+    current_theme_index = (current_theme_index + 1) % len(themes)
+    theme = themes[current_theme_index]
+    keychain.set_password('timer', 'timer', str(current_theme_index))
+    self.set_theme() 
+              
+  def set_theme(self):
+    self.background_color = theme.back
+    for b in self.times:
+      b.background_color = theme.back
+      b.tint_color = theme.front
+      b.objc_instance.layer().setShadowColor_(theme.shadow)
         
   def layout(self):
     insets = self.objc_instance.safeAreaInsets()
@@ -177,6 +185,14 @@ class TimeSelection(ui.View):
   
     cell_width = w/self.times_per_row
     cell_height = h/self.rows
+    #w,h = 0,0
+    dim = 0
+    for i in range(len(self.default_times)):
+      b = self.times[i]
+      b.size_to_fit()
+      dim = max(dim, b.width, b.height)
+      #w = max(w, b.width)
+      #h = max(h, b.height)
     for i in range(len(self.default_times)):
       b = self.times[i]
       column = i % self.times_per_row
@@ -189,7 +205,7 @@ class TimeSelection(ui.View):
       )
       b.frame = frame
       center = b.center
-      b.size_to_fit()
+      b.width = b.height = dim
       b.center = center
       #b.frame = b.frame.inset(10,10)
       
@@ -200,12 +216,12 @@ class TimeSelection(ui.View):
       self.navigation_view.pop_view()
     first_start_time = int(sender.title)
     l = ui.Label()
-    l.background_color = 'white'
-    l.text_color = 'darkgrey'
+    l.background_color = theme.back
+    l.text_color = theme.secondary
     l.text = str(first_start_time)
     l.alignment = ui.ALIGN_CENTER
         
-    l.font = ('Courier', timer_font_size)
+    l.font = (font, timer_font_size)
     l.touch_enabled = True
     t = Toucher(start_time=int(sender.title), frame=l.bounds, flex='WH')
     l.add_subview(t)
