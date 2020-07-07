9ef76973c594625be4a8b172805e1c965f17bba2
Mikael <mikael.honkala@gmail.com>
2020-07-07 22:51:38 +0300

    Better printing

--- brython.py
+++ brython.py
@@ -1,30 +1,10 @@
 import console
+from objc_util import on_main_thread, NSURL
 import editor
+import os.path
 
 import wkwebview
 
-
-console.clear()
-
-class BrythonRunner(wkwebview.WKWebView):
-    
-    def _message(self, message):
-        level, content = message['level'], message['content']
-        
-        if level not in ['code', 'raw']:
-            if content == 'using indexedDB for stdlib modules cache':
-                print('Brython started')
-                return
-            
-            if level.upper() == 'LOG':
-                print(content)
-                return
-        
-        super()._message(message)    
-          
-python_code = editor.get_text()
-
-wv = BrythonRunner()
 
 html_start = '''
 <!doctype html>
@@ -42,7 +22,7 @@ html_start = '''
 
 <script type="text/python">
 import sys
-print(f'Running Python {sys.version}')
+print(f'Running Python {sys.version}\\n\\n-----------------------------\\n')
 </script>
 
 <script type="text/python">
@@ -57,8 +37,44 @@ html_end = '''
 
 </html>
 '''
+    
 
-html = html_start + python_code + html_end
+def main():
+    console.clear()
+    
+    class BrythonRunner(wkwebview.WKWebView):
+        
+        @on_main_thread
+        def load_html(self, html):
+            # Need to set a base directory to get
+            # real js errors
+            current_working_directory = os.path.dirname(editor.get_path())
+            root_dir = NSURL.fileURLWithPath_(current_working_directory)
+            self.webview.loadHTMLString_baseURL_(html, root_dir)
+        
+        def _message(self, message):
+            level, content = message['level'], message['content']
+            
+            if level not in ['code', 'raw']:
+                if content == 'using indexedDB for stdlib modules cache':
+                    #print('Brython started')
+                    return
+                
+                if level.upper() == 'LOG':
+                    print(content,
+                    end=('' if content.endswith('\n') else '\n'))
+                    return
+            
+            super()._message(message)    
+              
+    python_code = editor.get_text()
+    
+    wv = BrythonRunner()
+    
+    html = html_start + python_code + html_end
+    
+    wv.load_html(html)
 
-wv.load_html(html)
-
+if __name__ == '__main__':
+    
+    main()
